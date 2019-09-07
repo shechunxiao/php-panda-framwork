bbfde34a4e306c088588cfea1a673a3b9a6ea18a
scx <282074579@qq.com>
2019-09-07 17:56:39 +0800

    提交

--- Index.php
+++ Index.php
@@ -1,12 +1,24 @@
 <?php
-
 require 'bootstrap/autoload.php';
 
 
 try {
-    $DB = new PDO('mysql:host=127.0.0.1;port=3306;dbname=shechunxiao;charset=UTF8;','root','', array(PDO::ATTR_PERSISTENT=>true));
-    $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $result = $DB->query('select * from first22');
+    $DB = new PDO('mysql:host=127.0.0.1;port=3306;dbname=shechunxiao;charset=UTF8;','root','', [
+        PDO::ATTR_PERSISTENT=>true
+    ]);
+    print $DB->getAttribute(PDO::ATTR_AUTOCOMMIT).'<br/>';
+    print $DB->getAttribute(PDO::ATTR_CASE).'<br/>';
+    print $DB->getAttribute(PDO::ATTR_CLIENT_VERSION).'<br/>';
+    print $DB->getAttribute(PDO::ATTR_CONNECTION_STATUS).'<br/>';
+    print $DB->getAttribute(PDO::ATTR_DRIVER_NAME).'<br/>';
+    print $DB->getAttribute(PDO::ATTR_ERRMODE).'<br/>';
+    print $DB->getAttribute(PDO::ATTR_ORACLE_NULLS).'<br/>';
+    print $DB->getAttribute(PDO::ATTR_PERSISTENT).'<br/>';
+    print $DB->getAttribute(PDO::ATTR_SERVER_INFO).'<br/>';
+    print $DB->getAttribute(PDO::ATTR_SERVER_VERSION).'<br/>';
+//    print $DB->getAttribute(PDO::ATTR_TIMEOUT).'<br/>'; //mysql不支持
+//    print $DB->getAttribute(PDO::ATTR_PREFETCH).'<br/>';  //mysql不支持
+    $result = $DB->query('select * from first');
     var_dump($result);
     print_r($result->fetchAll());
 } catch (PDOException $e) {
