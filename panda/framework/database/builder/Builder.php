7e23ef46d99a5e9dd9c4da5f0a543e2cfa085b16
scx <282074579@qq.com>
2019-11-08 15:07:09 +0800

    需要剥离as

--- panda/framework/database/builder/Builder.php
+++ panda/framework/database/builder/Builder.php
@@ -41,7 +41,7 @@ class Builder
     public function sqlForSelect(Query $query)
     {
         $sql = $this->dealSqlOrders($query);
-//        return 'select';
+        var_dump($sql);
     }
 
     /**
@@ -76,9 +76,46 @@ class Builder
         $sql = [];
         foreach ($this->sqlOrders as $key => $item) {
             if (!is_null($query->$item)) {
-                $sql[$item] = $this->{$item . 'Resolve'}($query->$item);
+                $sql[$item] = $this->{$item . 'Resolve'}($query->$item, $query);
             }
         }
+        return $sql;
+    }
+
+    /**
+     * 首先判断是否有as，有as则剥离出来再调用escapeValueNoAs
+     * @param $value
+     * @return string
+     */
+    public function escapeValue($value)
+    {
+        if (($pos = $this->posProcessAs($value)) != false) {
+            return $pos;
+        }
+        return $this->escapeValueNoAs($value);
+    }
+
+    /**
+     * 判断$value中的as的位置
+     * @param $value
+     * @return bool
+     */
+    public function posProcessAs($value)
+    {
+        return strpos($value, 'as');
+    }
+
+    /**
+     * 转义相应的值
+     * @param $value
+     * @return string
+     */
+    public function escapeValueNoAs($value)
+    {
+        if ($value === '*') {
+            return $value;
+        }
+        return '`' . str_replace('`', '``', $value) . '`';
     }
 
     /**
@@ -86,7 +123,7 @@ class Builder
      * @param $item
      * @return int
      */
-    public function aggregateResolve($item)
+    public function aggregateResolve($item, $query)
     {
         return 111;
     }
@@ -98,25 +135,35 @@ class Builder
      */
     public function tableResolve($item)
     {
-        return 'from ' . $item;
+        return 'from ' . $this->escapeValue($item);
     }
 
     /**
      * 解析所选字段
      * @param $item
+     * @return string
      */
     public function fieldsResolve($item)
     {
-
+        foreach ($item as &$field) {
+            $field = $this->escapeValue($field);
+        }
+        $fields = implode(',', $item);
+        return 'select ' . $fields;
     }
 
     /**
      * 解析关联查询
      * @param $item
+     * @return string
      */
     public function joinsResolve($item)
     {
-
+        $joinString = '';
+        foreach ($item as $join) {
+            $joinString .= $join['type'] . ' join ' . $this->escapeValue($join['table']) . ' on ' . $join['first'] . '=' . $join['second'];
+        }
+        return $joinString;
     }
 
     /**
--- public/index.php
+++ public/index.php
@@ -6,12 +6,38 @@ require dirname(__DIR__) . '/bootstrap/autoload.php';
 
 $app = new \Panda\foundation\Application(dirname(__DIR__));
 
+///**
+// * 测试PDO prepare
+// */
+//$dbh = new PDO('mysql:host=localhost;dbname=shechunxiao', 'root', '');
+//$sql = 'select * from first';
+//$statement = $dbh->prepare($sql);
+//$statement->execute();
+////var_dump($statement->fetchAll());
+//
+//function wrapValue($value)
+//{
+//    if ($value === '*') {
+//        return $value;
+//    }
+//    return '`'.str_replace('`', '``', $value).'`';
+//}
+
+
+
+
+
+//$a = 'id';
+//function tr($it){
+//    return "`".$it."`";
+//}
+//var_dump(tr($a));die();
 //$data = \Panda\facade\Db::table('first')->group('id','name','age')->where('id','>',50)->select();
 //$where['id'] = [
 //    ['>',1,'and'],
 //    ['<',10,'or']
 //];
-//$where['id'] = ['>',1];
+$where['id'] = ['>',1];
 $where[] = ['id', '>', 324232];
 $where[] = ['name', '=', '张三'];
 $where2['test'] = ['>', 100];
@@ -24,10 +50,9 @@ $where3['last'] = [
 //->where($where)
 //where($where2)
 //->where($where3)
-$data = \Panda\facade\Db::table('first')->field('id','inter')->where($where2)->where($where3)->whereOr('inner','>',1)->select();
+$data = \Panda\facade\Db::table('first as f')->field('count(id) as mycount','inter')
+    ->join('first_extend as fe','fe.first_id','=','f.id','left')
+    ->where($where2)->where($where3)->whereOr('inner','>',1)->select();
 //var_dump($data);
 
-
-//var_dump($where1);
-//$where3[] = ['id','>',1];
-//var_dump($where3);
+;
