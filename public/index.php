a1e196da8f67b748617dc78a13fb813cd865e29f
scx <282074579@qq.com>
2019-11-25 17:59:44 +0800

    构建服务容器和启动，路由等实现功能

--- public/index.php
+++ public/index.php
@@ -7,3 +7,12 @@ require dirname(__DIR__) . '/bootstrap/autoload.php';
 $app = new \Panda\foundation\Application(dirname(__DIR__));
 
 //研究路由的实现以及Exception报错和日志系统的实现方式
+
+/**
+ * 基本构建思路:
+ *      1.服务提供者，必须有一个基类,所有的关键服务核心类都继承这个类
+ *      2.利用http-foundation 扩展实现http请求的处理
+ *      3.执行各个服务的boot方法，启动，原来只是注册
+ *      4.通过管道模式分发请求到执行的控制器和方法
+ *      5.最后用echo $content输出内容到浏览器上
+ */
